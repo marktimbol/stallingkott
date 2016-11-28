ca3c6459568cf430fff91dfdb1568aa6719ca1ab
Mark Timbol <mark.timbol@hotmail.com>
2016-11-28 14:40:04 +0400

    Update

--- resources/views/public/clients/index.blade.php
+++ resources/views/public/clients/index.blade.php
@@ -15,19 +15,130 @@
 
 @section('content')
 	<div class="container">
-		<div class="row">
+		<div class="row Clients">
 			<div class="col-md-12">
-				<p>&nbsp;</p>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</p>
-				<p>&nbsp;</p>
+				<h3 class="fancy-title">Customers we are dealing with</h3>
 			</div>
+
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<ul>
+							<li>Raffles Dubai</li>
+							<li>Shangri-La Dubai</li>
+							<li>Shangri-La A.D</li>
+							<li>Sheraton Mall of the Emirates</li>
+							<li>Taj Palace</li>
+							<li>Viceroy Hotel (Yasisland)</li>
+							<li>Al Diar Palm (A.D)</li>
+							<li>Movenpick Deira</li>
+							<li>Movenpick IBN Battuta</li>
+							<li>Sheraton Abu Dhabi</li>
+							<li>Holiday Inn Express Media City</li>
+							<li>Radisson Blu Media City</li>
+							<li>Crowne Plaza (Yasisland)</li>
+							<li>Crown Plaza (A.D)</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-body">			
+						<ul>
+							<li>One & Only The Palm</li>
+							<li>One & Only Royal Mirage</li>
+							<li>Pearl Residence Hotel Burdubai</li>
+							<li>Atlants the palm</li>
+							<li>Address Dubai Marina</li>
+							<li>Meyden Hotel</li>
+							<li>Yasisland Rotana A.D</li>
+							<li>Centor al Manha A.D</li>
+							<li>Novotel Albustan A.D</li>
+							<li>Al Raha Beach Hotel A.d</li>
+							<li>Tulip Inn Sharjah</li>
+							<li>Hilton Sharjah</li>
+							<li>Arabian Park</li>
+							<li>Beach Rotana(A.D)</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-body">	
+						<ul>
+							<li>Al Habtoor Engineering (A.D)</li>
+							<li>Al Maha Arjaan Rotana</li>
+							<li>Al Hayath Real Estate (A.D)</li>
+							<li>Arjaan Rotana Dubai</li>
+							<li>Avari Al Barsha</li>
+							<li>Al Manzel Hotel (A.D)</li>
+							<li>Al diar siji Apt fujairah</li>
+							<li>Bonnington Jumeirah</li>
+							<li>Burjuman Arjaan Rotana</li>
+							<li>Brook Field Multiplex (A.D)</li>
+						</ul>
+					</div>
+				</div>			
+			</div>
+
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<ul>
+							<li>Citymax Al barsha</li>
+							<li>Cristal Hotel (A.D)</li>
+							<li>City Seasons</li>
+							<li>Conrad</li>
+							<li>Danat Alain</li>
+							<li>Emerald Properties</li>
+							<li>Golden Sands</li>
+							<li>Grand Hotel SHJ</li>
+							<li>Grosvenor House 1</li>
+							<li>Grosvenor House 2</li>
+						</ul>
+					</div>
+				</div>			
+			</div>	
+
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<ul>
+							<li>Hilton Baynunah(A.D)</li>
+							<li>Holiday International SHJ</li>
+							<li>Hyatt Regency</li>
+							<li>Hilton Alain</li>
+							<li>Imperial Hotel Burdubai</li>
+							<li>Imperial Suites Hotel</li>
+							<li>King Gate Hotel A.D)</li>
+							<li>Le Merdien (Fujairah)</li>
+							<li>Sheraton Jumeirah</li>
+						</ul>
+					</div>
+				</div>			
+			</div>	
+
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<ul>
+							<li>LouLoua Beach Resort</li>
+							<li>Mercure Hotel(A.D)</li>
+							<li>Movenpick Deira</li>
+							<li>Movenpick Jumeirah</li>
+							<li>Mc Gattinga (A.D)</li>
+							<li>Ramada Sharjah Hotel</li>
+							<li>Radisson Blu Down town</li>
+							<li>Radisson Royal</li>
+							<li>Sheraton Abudhabi</li>
+						</ul>
+					</div>
+				</div>			
+			</div>				
 		</div>
 	</div>
 @endsection
